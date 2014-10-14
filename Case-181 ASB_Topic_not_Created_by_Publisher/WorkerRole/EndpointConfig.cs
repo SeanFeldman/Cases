08bf5494034dff6d9e30b02d21326c7fd7ec0e43
Sean Feldman <feldman.sean@gmail.com>
2014-10-13 21:40:51 -0600

    Cloud service implementation

---
+++ Case-181 ASB_Topic_not_Created_by_Publisher/WorkerRole/EndpointConfig.cs
@@ -0,0 +1,29 @@
+﻿using NServiceBus;
+
+namespace WorkerRole
+{
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Worker
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.UseTransport<AzureServiceBusTransport>();
+            configuration.UsePersistence<AzureStoragePersistence>();
+
+            configuration.Conventions()
+                .DefiningEventsAs(t => t.Namespace != null && t.Namespace.StartsWith("Case181.Messages.Events"));
+
+            configuration.EndpointName("Azure.CloudServiceExample.Server");
+        }
+    }
+
+    public class Test : IWantToRunWhenBusStartsAndStops
+    {
+        public void Start()
+        {
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}

9ad008c61ce0f76fd183c1aebf37bb12fd165c33
Sean Feldman <feldman.sean@gmail.com>
2014-11-18 23:31:55 -0700

    reproduction

---
+++ Case-210 Azure_Host_throws_ArgumentNullException/Case_210_Endpoint/EndpointConfig.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+
+namespace Case_210_Endpoint_V5
+{
+    public class EndpointConfig : IConfigureThisEndpoint
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.EndpointName("Case_210_Endpoint");
+            configuration.UsePersistence<InMemoryPersistence>();
+            var sendOnlyBus = Bus.CreateSendOnly(configuration);
+            (sendOnlyBus as IStartableBus).Start();
+        }
+    }
+}

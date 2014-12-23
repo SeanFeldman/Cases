389ce4f751c2cbdac378e0210b1aa38920c8fae1
Sean Feldman <feldman.sean@gmail.com>
2014-12-22 17:49:52 -0700

    EP config file

---
+++ Case-223 Azure_Cloud_Service_Web_Role_config_problem/WorkerRole/EndpointConfig.cs
@@ -0,0 +1,16 @@
+﻿using Case_223.Shared;
+using NServiceBus;
+
+namespace WorkerRole
+{
+    class EndpointConfig : IConfigureThisEndpoint, AsA_Worker
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            //configuration.AzureConfigurationSource();
+            configuration.UseTransport<AzureServiceBusTransport>();
+            configuration.UsePersistence<AzureStoragePersistence>();
+            configuration.ApplyMessageConventions();
+        }
+    }
+}

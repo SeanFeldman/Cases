738e10cf9180d5dbe103606506fb13dd6ee0a4c8
Sean Feldman <feldman.sean@gmail.com>
2015-04-28 18:27:40 -0600

    Case-10108 XmlSerializer-fails-to-deserialize-array-of-nulluble-ints
    NSB Core  5.2.0

---
+++ Case-10108 XmlSerializer-fails-to-deserialize-array-of-nulluble-ints/Receiver/ReceiverProgram.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Messages;
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using NServiceBus.Features;
+
+namespace Receiver
+{
+    class ReceiverProgram
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(500);
+            var configuration = new BusConfiguration();
+            configuration.EndpointName("case-10108-receiver");
+            configuration.UseTransport<MsmqTransport>();
+            configuration.UsePersistence<InMemoryPersistence>();
+            configuration.DisableFeature<SecondLevelRetries>();
+            configuration.DisableFeature<TimeoutManager>();
+            configuration.DisableFeature<AutoSubscribe>();
+            var bus = Bus.Create(configuration);
+            bus.Start();
+            Console.WriteLine("Receiver is running...");
+            keepRunning = true;
+            while (keepRunning)
+            {
+            }
+        }
+
+        public static bool keepRunning { get; set; }
+    }
+
+    public class CommandWithArrayOfNullablesHandler : IHandleMessages<CommandWithArrayOfNullables>
+    {
+        public void Handle(CommandWithArrayOfNullables message)
+        {
+            Console.WriteLine("Received a message");
+            foreach (var value in message.Values)
+            {
+                Console.WriteLine("item: '{0}'", value);
+            }
+        }
+    }
+
+    class ConfigureErrorQueue : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "error",
+            };
+        }
+    }
+
+    class ConfigureMaxRetries : IProvideConfiguration<TransportConfig>
+    {
+        public TransportConfig GetConfiguration()
+        {
+            return new TransportConfig
+            {
+                MaxRetries = 1,
+                MaximumConcurrencyLevel = 1
+            };
+        }
+    }
+}
---
+++ Case-10108 XmlSerializer-fails-to-deserialize-array-of-nulluble-ints/Sender/SenderProgram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Messages;
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using NServiceBus.Features;
+
+namespace Sender
+{
+    class SenderProgram
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new BusConfiguration();
+            configuration.EndpointName("case-10108-sender");
+            configuration.UseTransport<MsmqTransport>();
+            configuration.UsePersistence<InMemoryPersistence>();
+            configuration.DisableFeature<SecondLevelRetries>();
+            configuration.DisableFeature<TimeoutManager>();
+            configuration.DisableFeature<AutoSubscribe>();
+            var bus = Bus.Create(configuration);
+            bus.Start();
+            bus.Send(new CommandWithArrayOfNullables { Values = new int?[] {1, 2, 3}});
+        }
+    }
+
+    class ConfigureRouting : IProvideConfiguration<UnicastBusConfig>
+    {
+        public UnicastBusConfig GetConfiguration()
+        {
+            return new UnicastBusConfig
+            {
+                MessageEndpointMappings = new MessageEndpointMappingCollection
+                {
+                    new MessageEndpointMapping
+                    {
+                        AssemblyName = "Messages",
+                        Namespace = "Messages",
+                        Endpoint = "case-10108-receiver"
+                    }
+                }
+            };
+        }
+    }
+
+    class ConfigureErrorQueue : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+    {
+        public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+        {
+            return new MessageForwardingInCaseOfFaultConfig
+            {
+                ErrorQueue = "error",
+            };
+        }
+    }
+
+    class ConfigureMaxRetries : IProvideConfiguration<TransportConfig>
+    {
+        public TransportConfig GetConfiguration()
+        {
+            return new TransportConfig
+            {
+                MaxRetries = 1,
+                MaximumConcurrencyLevel = 1
+            };
+        }
+    }
+}

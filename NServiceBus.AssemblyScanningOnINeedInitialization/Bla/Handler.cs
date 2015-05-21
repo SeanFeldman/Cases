dcf488985ddd13f8015b23330980d5067e6a7497
Sean Feldman <feldman.sean@gmail.com>
2015-05-21 11:57:15 -0600

    assembly scanning options not taken into account during INeedInitialization execution

---
+++ NServiceBus.AssemblyScanningOnINeedInitialization/Bla/Handler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using Shared;
+
+namespace Blaa
+{
+    public class Handler : IHandleMessages<TestCommand>
+    {
+        public void Handle(TestCommand message)
+        {
+            Console.WriteLine("got it");
+        }
+    }
+}
+
+public class CreateErrorQueue : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+{
+    public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+    {
+        return new MessageForwardingInCaseOfFaultConfig
+        {
+            ErrorQueue = "error"
+        };
+    }
+}
---
+++ NServiceBus.AssemblyScanningOnINeedInitialization/NServiceBus.AssemblyScanningOnINeedInitialization/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NServiceBus;
+using NServiceBus.Config;
+using NServiceBus.Config.ConfigurationSource;
+using Shared;
+
+namespace NServiceBus.AssemblyScanningOnINeedInitialization
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new BusConfiguration();
+            config.UsePersistence<InMemoryPersistence>();
+            config.EndpointName("nservicebus");
+            config.EnableInstallers();
+            //config.AssembliesToScan(AllAssemblies.Except("Blaa.dll"));
+            var bus = Bus.Create(config);
+            bus.Start();
+            bus.SendLocal(new TestCommand());
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                
+            }
+        }
+    }
+}
+
+public class Bla : INeedInitialization
+{
+    public void Customize(BusConfiguration config)
+    {
+        config.AssembliesToScan(AllAssemblies.Except("Blaa.dll"));
+        config.EndpointName("ops");
+    }
+}
+
+public class CreateErrorQueue : IProvideConfiguration<MessageForwardingInCaseOfFaultConfig>
+{
+    public MessageForwardingInCaseOfFaultConfig GetConfiguration()
+    {
+        return new MessageForwardingInCaseOfFaultConfig
+        {
+            ErrorQueue = "error"
+        };
+    }
+}

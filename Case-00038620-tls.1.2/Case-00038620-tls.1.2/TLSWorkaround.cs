b0bd1ee2f94ab57d4d7f758a28b0dc2471d77c38
Sean Feldman <feldman.sean@gmail.com>
2018-08-13 15:18:20 -0600

    Add case 00038620 with workaround for TLS1.2 issue

---
+++ Case-00038620-tls.1.2/Case-00038620-tls.1.2/TLSWorkaround.cs
@@ -0,0 +1,19 @@
+﻿namespace Case_00038620_tls._1._2
+{
+    using System.Net;
+    using NServiceBus;
+    using NServiceBus.Logging;
+
+    public class TLSWorkaround : INeedInitialization
+    {
+        private static ILog logger = LogManager.GetLogger<TLSWorkaround>();
+
+        public void Customize(BusConfiguration configuration)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.CheckCertificateRevocationList = false;
+            ServicePointManager.ServerCertificateValidationCallback = (a, b, c, d) => true;
+            logger.Warn("TLS 1.2 workaround applied.");
+        }
+    }
+}
